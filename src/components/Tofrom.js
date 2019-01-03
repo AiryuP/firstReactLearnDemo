35973a76c5be98345f63f5a27115bc95d9fe701d
pengyu <airpyu@163.com>
2019-01-03 18:21:49 +0800

    表单完成

--- src/components/Tofrom.js
+++ src/components/Tofrom.js
@@ -7,6 +7,7 @@ class Tofrom extends React.Component {
             msg: 'react表单',
             name: '',
             sex: '1',
+            city: '',
             citys:[
                 '北京','上海','深圳'
             ],
@@ -31,7 +32,7 @@ class Tofrom extends React.Component {
     handelSubmit = (e)=>{
         // 组织submit的提交事件（from 的默认事件）
         e.preventDefault();
-        console.log( this.state.name ,this.state.sex)
+        console.log( this.state.name ,this.state.sex,this.state.city,this.state.hobby,this.state.info)
     }
 
     handelName =(e)=>{
@@ -42,6 +43,27 @@ class Tofrom extends React.Component {
     handelSex =(e)=>{
         this.setState({
             sex : e.target.value
+        })
+    }
+
+    handelCity = (e)=>{
+        this.setState ({
+            city: e.target.value
+        })
+    }
+
+    handelBobby = (key)=>{
+        // this.setState.hoby = e.target.value
+        var hobby = this.state.hobby;
+        hobby[key].checked = !hobby[key].checked;
+        this.setState({
+            hobby:hobby
+        })
+    }
+
+    handelInfo = (e)=>{
+        this.setState({
+            info: e.target.value
         })
     }
 
@@ -58,6 +80,32 @@ class Tofrom extends React.Component {
                     用户名：<input type="text" value={ this.state.name } onChange={ this.handelName } /> <br/><br/>
                     性  别：男<input type="radio" value="1" checked={ this.state.sex == 1 } onChange={ this.handelSex } /> 
                            女<input type="radio" value="0"  checked={ this.state.sex == 0 } onChange={ this.handelSex } /> <br/><br/>
+                    居住城市：
+                        <select value={ this.state.city } onChange={ this.handelCity }>
+                        
+                            {
+                                this.state.citys.map(function(value,key){
+                                    return <option key={ key }  >{ value }</option>
+                                })
+                            }
+
+                        </select> <br/><br/>
+                    爱好:
+                            {
+                                this.state.hobby.map((value,key)=>{
+                                    return (
+                                        <span key={ key } > 
+                                        
+                                           <input type="checkbox" checked={ value.checked } onChange={ this.handelBobby.bind( this,key ) } name="" id=""/>  { value.title } 
+
+                                        </span>
+                                    )
+                                })
+                                
+                            }
+                            <br/><br/>
+                    备注：
+                        <textarea value={ this.state.info } onChange={ this.handelInfo } name="" id="" cols="30" rows="10"></textarea>
 
                     <br/>
                     <input type="submit" value="提交"  />
